3c66b710e084af5824d6069c1d1773870af8607d
Owais <owais.quadri@ontariotechu.net>
2021-03-20 20:29:14 -0400

    movement mechanics and joining tiles

--- main.py
+++ main.py
@@ -54,32 +54,177 @@ class Game(object):
         mt=[]
         curr=0
         for sq in self.board:
-            print(curr,": ", sq)
             if sq == 0:
                 mt.append(curr)
             curr+=1
         if len(mt) != 0:
             index=random.choice(mt)
-            print(index)
+            
             self.board[index]=random.choice([2,4])
         else:
             #partly game over (game is over when there are no more possible moves) 
             print("GameOver!")
-        self.prnt()
+    #movement
+    def swipe(self,dir):
+        joined=False
+        #create empty list L and B
+        L=[]
+        B=self.board
+        if dir=="up":
+            #then
+            # 0 < 4 < 8 < 12
+            # loop through list by columns
+            # we want order 0,4,8,12,  1,5,9,13,  2,6,10,14,  3,7,11,15
+            for i_col in range(4):
+                for i_row in range (4):
+                    #loop varaible index
+                    index=i_col+(i_row*4)
+                    #check if curr == (cur -1), add together and make joined=true
+                    if self.board[index] != 0:#if not zero
+                        if (len(L) <1) or (joined==True):#if size <= 1,
+                            L.append(self.board[index])#joined = false and append to blank list L
+                            joined=False
+                        else:#if size > 1, and joined == False
+                            if self.board[index] == L[len(L)-1] :#if current is equal to latest entry in L
+                                L[len(L)-1]+=self.board[index]#add current to latest entry in L
+                                joined=True#therefore joinging them (joined=True)
+                            else:
+                                #append the value as a new element to L
+                                L.append(self.board[index])
+                                #and joined remains False
+                                joined=False
+                #outside one loop,not both:
+                #add current combined column/row (L) to overall board list B and empty L
+                #starting at 0 and going up by 4 for output
+                out=0
+                while out <= 12 :
+                    if len(L)!=0 :#if L has elements
+                        #put them at the index i_col+out of B and remove from L
+                        B[i_col+out]=L.pop(0)
+                    else:#put 0 at same index
+                        B[i_col+out]=0
+                    out+=4
+        elif dir == "down":
+            #then
+            # 0 > 4 > 8 > 12
+            # loop through list by columns
+            # we want order 12,8,4,0,  ...
+            for i_col in range(4):
+                for i_row in range (4):
+                    #loop varaible index
+                    index=i_col+(12-i_row*4)
+                    #check if curr == (cur -1), add together and make joined=true
+                    if self.board[index] != 0:#if not zero
+                        if (len(L) <1) or (joined==True):#if size <= 1,
+                            L.append(self.board[index])#joined = false and append to blank list L
+                            joined=False
+                        else:#if size > 1, and joined == False
+                            if self.board[index] == L[len(L)-1] :#if current is equal to latest entry in L
+                                L[len(L)-1]+=self.board[index]#add current to latest entry in L
+                                joined=True#therefore joinging them (joined=True)
+                            else:
+                                #append the value as a new element to L
+                                L.append(self.board[index])
+                                #and joined remains False
+                                joined=False
+                #outside one loop,not both:
+                #add current combined column/row (L) to overall board list B and empty L
+                #starting at 12 and going down by 4 for output
+                out=12
+                while out >= 0 :
+                    if len(L)!=0 :#if L has elements
+                        #put them at the index i_col+out of B and remove from L
+                        B[i_col+out]=L.pop(0)
+                    else:#put 0 at same index
+                        B[i_col+out]=0
+                    out-=4
+        elif dir == "right":
+            #then
+            # 0 > 1 > 2 > 3
+            # loop through list by columns
+            # we want order 0,1,2,3,  ...
+            for i_row in range(4):
+                for i_col in range (4):
+                    #loop varaible index
+                    index=(3-i_col)+(i_row*4)
+                    #check if curr == (cur -1), add together and make joined=true
+                    if self.board[index] != 0:#if not zero
+                        if (len(L) <1) or (joined==True):#if size <= 1,
+                            L.append(self.board[index])#joined = false and append to blank list L
+                            joined=False
+                        else:#if size > 1, and joined == False
+                            if self.board[index] == L[len(L)-1] :#if current is equal to latest entry in L
+                                L[len(L)-1]+=self.board[index]#add current to latest entry in L
+                                joined=True#therefore joinging them (joined=True)
+                            else:
+                                #append the value as a new element to L
+                                L.append(self.board[index])
+                                #and joined remains False
+                                joined=False
+                #outside one loop,not both:
+                #add current combined column/row (L) to overall board list B and empty L
+                #starting at 0 and going up by 1 for output
+                out=3
+                while out >= 0 :
+                    if len(L)!=0 :#if L has elements
+                        #put them at the index i_col+out of B and remove from L
+                        B[i_row*4+out]=L.pop(0)
+                    else:#put 0 at same index
+                        B[i_row*4+out]=0
+                    out-=1
+
+        elif dir == "left":
+            #then
+            # 0 > 1 > 2 > 3
+            # loop through list by columns
+            # we want order 0,1,2,3,  ...
+            for i_row in range(4):
+                for i_col in range (4):
+                    #loop varaible index
+                    index=i_col+(i_row*4)
+                    #check if curr == (cur -1), add together and make joined=true
+                    if self.board[index] != 0:#if not zero
+                        if (len(L) <1) or (joined==True):#if size <= 1,
+                            L.append(self.board[index])#joined = false and append to blank list L
+                            joined=False
+                        else:#if size > 1, and joined == False
+                            if self.board[index] == L[len(L)-1] :#if current is equal to latest entry in L
+                                L[len(L)-1]+=self.board[index]#add current to latest entry in L
+                                joined=True#therefore joinging them (joined=True)
+                            else:
+                                #append the value as a new element to L
+                                L.append(self.board[index])
+                                #and joined remains False
+                                joined=False
+                #outside one loop,not both:
+                #add current combined column/row (L) to overall board list B and empty L
+                #starting at 0 and going up by 1 for output
+                out=0
+                while out <= 3 :
+                    if len(L)!=0 :#if L has elements
+                        #put them at the index i_col+out of B and remove from L
+                        B[i_row*4+out]=L.pop(0)
+                    else:#put 0 at same index
+                        B[i_row*4+out]=0
+                    out+=1
+        else:
+            #else do something i guess
+            print("else")
+
+        #once the loop completes, outside all the if/elif/else statements:
+        # do g.setBoard with B, which is the overall board list
+        self.set_board(B)
     
     #board getter setter
     def get_board(self):
         return self.board
     def set_board(self,b):
-        if b == self.board:
-            moved=False
-        else:
-            this.board = b 
-            this.moved=True
+            self.board = b 
+            self.moved=True
     #gameover setter getter
-    def setGame(self, xyz):
+    def setGameOver(self, xyz):
         self.isGameOver=xyz
-    def getGame(self):
+    def getGameOver(self):
         return self.isGameOver
     #moved setter getter
     def setMoved(self, xyz):
@@ -108,71 +253,77 @@ class main(object):
                 #draw lettering
                 num= inGameFont.render("", 1, FONT_24)
                 screen.blit(num, ((fx+(x*90))+5, (fy+(y*90))+25))
-            if val == 2 :
+            elif val == 2 :
                 #draw box 
                 pygame.draw.rect(screen,BG_2,[(fx+(x*size)),(fy+(y*size)),size,size])
                 #draw lettering
-                num= inGameFont.render("2", 1, FONT_24)
+                num= inGameFont.render(str(val), 1, FONT_24)
                 screen.blit(num, ((fx+(x*size-3*x))+45, (fy+(y*size-3*y))+35))
-            if val == 4 :
+            elif val == 4 :
                 #draw box 
                 pygame.draw.rect(screen,BG_4,[(fx+(x*size)),(fy+(y*size)),size,size])
                 #draw lettering
-                num= inGameFont.render("4", 1, FONT_24)
+                num= inGameFont.render(str(val), 1, FONT_24)
                 screen.blit(num, ((fx+(x*size-3*x))+45, (fy+(y*size-3*y))+35))
-            if val == 8 :
+            elif val == 8 :
                 #draw box 
                 pygame.draw.rect(screen,BG_8,[(fx+(x*size)),(fy+(y*size)),size,size])
                 #draw lettering
-                num= inGameFont.render("8", 1, FONT_8PLUS)
+                num= inGameFont.render(str(val), 1, FONT_8PLUS)
                 screen.blit(num, ((fx+(x*size-3*x))+45, (fy+(y*size-3*y))+35))
-            if val == 16 :
+            elif val == 16 :
                 #draw box 
                 pygame.draw.rect(screen,BG_16,[(fx+(x*size)),(fy+(y*size)),size,size])
                 #draw lettering
-                num= inGameFont.render("16", 1, FONT_8PLUS)
+                num= inGameFont.render(str(val), 1, FONT_8PLUS)
                 screen.blit(num, ((fx+(x*size-3*x))+39, (fy+(y*size-3*y))+35))
-            if val == 32 :
+            elif val == 32 :
                 #draw box 
                 pygame.draw.rect(screen,BG_32,[(fx+(x*size)),(fy+(y*size)),size,size])
                 #draw lettering
-                num= inGameFont.render("32", 1, FONT_8PLUS)
+                num= inGameFont.render(str(val), 1, FONT_8PLUS)
                 screen.blit(num, ((fx+(x*size-3*x))+39, (fy+(y*size-3*y))+35))
-            if val == 64 :
+            elif val == 64 :
                 #draw box 
                 pygame.draw.rect(screen,BG_64,[(fx+(x*size)),(fy+(y*size)),size,size])
                 #draw lettering
-                num= inGameFont.render("64", 1, FONT_8PLUS)
+                num= inGameFont.render(str(val), 1, FONT_8PLUS)
                 screen.blit(num, ((fx+(x*size-3*x))+39, (fy+(y*size-3*y))+35))
-            if val == 128 :
+            elif val == 128 :
                 #draw box 
                 pygame.draw.rect(screen,BG_128,[(fx+(x*size)),(fy+(y*size)),size,size])
                 #draw lettering
-                num= inGameFont.render("128", 1, FONT_8PLUS)
+                num= inGameFont.render(str(val), 1, FONT_8PLUS)
                 screen.blit(num, ((fx+(x*size-3*x))+33, (fy+(y*size-3*y))+35))
-            if val == 256 :
+            elif val == 256 :
                 #draw box 
                 pygame.draw.rect(screen,BG_256,[(fx+(x*size)),(fy+(y*size)),size,size])
                 #draw lettering
-                num= inGameFont.render("256", 1, FONT_8PLUS)
+                num= inGameFont.render(str(val), 1, FONT_8PLUS)
                 screen.blit(num, ((fx+(x*size-3*x))+33, (fy+(y*size-3*y))+35))
-            if val == 512 :
+            elif val == 512 :
                 #draw box 
                 pygame.draw.rect(screen,BG_512,[(fx+(x*size)),(fy+(y*size)),size,size])
                 #draw lettering
-                num= inGameFont.render("512", 1, FONT_8PLUS)
+                num= inGameFont.render(str(val), 1, FONT_8PLUS)
                 screen.blit(num, ((fx+(x*size-3*x))+33, (fy+(y*size-3*y))+35))
-            if val == 1024 :
+            elif val == 1024 :
                 #draw box 
                 pygame.draw.rect(screen,BG_1024,[(fx+(x*size)),(fy+(y*size)),size,size])
                 #draw lettering
-                num= inGameFont.render("1024", 1, FONT_8PLUS)
+                num= inGameFont.render(str(val), 1, FONT_8PLUS)
                 screen.blit(num, ((fx+(x*size-3*x))+25, (fy+(y*size-3*y))+35))
-            if val == 2048 :
+            elif val == 2048 :
                 #draw box 
                 pygame.draw.rect(screen,BG_2048,[(fx+(x*size)),(fy+(y*size)),size,size])
                 #draw lettering
-                num= inGameFont.render("2048", 1, FONT_8PLUS)
+                num= inGameFont.render(str(val), 1, FONT_8PLUS)
+                screen.blit(num, ((fx+(x*size-3*x))+25, (fy+(y*size-3*y))+35))
+            else:
+                #draw box 
+                pygame.draw.rect(screen,BG_HIGH,[(fx+(x*size)),(fy+(y*size)),size,size])
+                #draw lettering
+                num= inGameFont.render(str(val), 1, FONT_8PLUS)
                 screen.blit(num, ((fx+(x*size-3*x))+25, (fy+(y*size-3*y))+35))
             
         #inf loop for game
@@ -204,31 +355,29 @@ class main(object):
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if event.key == ord('w'):
-                        g.addTile()
-                        print("up")
+                        g.swipe("up")
+                        if (g.getMoved()):
+                            g.addTile()
                     if event.key == ord('s'):
-                        g.addTile()
-                        print("down")
+                        g.swipe("down")
+                        if (g.getMoved()):
+                            g.addTile()
                     if event.key == ord('d'):
-                        g.addTile()
-                        print("right")
+                        g.swipe("right")
+                        if (g.getMoved()):
+                            g.addTile()
                     if event.key == ord('a'):
-                        g.addTile()
-                        print("left")
+                        g.swipe("left")
+                        if (g.getMoved()):
+                            g.addTile()
                     if event.key == ord(' '):
                         g.restart()
-                        print("spc")
-                if event.type == pygame.KEYUP:
-                    if event.key == ord('w'):
-                        print("up-release")
-                    if event.key == ord('s'):
-                        print("down-release")
-                    if event.key == ord('d'):
-                        print("right-release")
-                    if event.key == ord('a'):
-                        print("left-release")
-                    if event.key == ord(' '):
-                        print("spc-release")
+                # if event.type == pygame.KEYUP:
+                #     if event.key == ord('w'):
+                #     if event.key == ord('s'):
+                #     if event.key == ord('d'):
+                #     if event.key == ord('a'):
+                #     if event.key == ord(' '):
                 if event.type == pygame.QUIT:
                     pygame.quit() 
                     exit(0)
